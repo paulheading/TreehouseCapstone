67bfa1ad88ac0dccde8fcca4a98412e599797021
Paul Heading <paul.heading@barbican.org.uk>
2020-08-02 13:00:42 +0100

    hooked up jawsDB

--- app.js
+++ app.js
@@ -4,22 +4,21 @@
 
 // https://appdividend.com/2019/06/06/what-is-process-env-in-node-js-environment-variables-in-node-js/
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
 let express = require("express");
 let app = express();
 let path = require("path");
-let buildPath;
+let buildPath = path.join(__dirname, "client/build");
+
+// .env context switching; if heroku needs different production settings
 
 // if (process.env.NODE_ENV !== 'production') {
 //   buildPath = path.join(__dirname, 'build');
 // } else {
-  
 // }
-
-buildPath = path.join(__dirname, 'client/build');
 
 // console.log(process.env);
 // console.log(process.env.NODE_ENV);
@@ -35,12 +34,8 @@ app.listen(process.env.PORT || 5000, function () {
   console.log("Node app is working!");
 });
 
-app.get('/params', (req, res) => {
+app.get("/params", (req, res) => {
   res.status(200).json(process.env);
-});
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(buildPath, 'index.html'));
 });
 
 // **********************************************************************
@@ -49,11 +44,20 @@ app.get('/', (req, res) => {
 
 // https://stackabuse.com/using-sequelize-orm-with-nodejs-and-express/
 
-const Sequelize = require("sequelize"),
-  sequelize = new Sequelize({
-    dialect: "sqlite",
-    storage: "database.sqlite",
-  });
+const Sequelize = require("sequelize");
+
+// local database setup
+
+// const sequelize = new Sequelize({
+//   dialect: "sqlite",
+//   storage: "database.sqlite",
+// });
+
+// live database setup
+
+const sequelize = new Sequelize(
+  "mysql://tf4yp7nya0dny6gq:oj3rlwdr0w078369@e764qqay0xlsc4cz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/njm1oz2jrc6xnqkq"
+);
 
 sequelize
   .authenticate()
@@ -87,6 +91,16 @@ sequelize.sync({ force: true }).then(() => {
 
 app.get("/notes", (req, res) => {
   Note.findAll().then((notes) => res.json(notes));
+});
+
+app.get("/api/getList", (req, res) => {
+  var list = ["item1", "item2", "item3"];
+  res.json(list);
+  console.log("Sent list of items");
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(buildPath, "index.html"));
 });
 
 // Place wildcard routes last *******************************************
